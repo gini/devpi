969e3e9875ddb43148baffb44e183634c7336cbc
David González <godboole@gmail.com>
2018-01-18 16:30:55 +0100

    Delete OSX sed remnants

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,8 @@ renderDockerfile () {
   cp -a Dockerfile.template $VERSION/Dockerfile
   cp -a files/requirements.txt.tpl $VERSION/requirements.txt
   cp -a scripts/entrypoint.sh $VERSION/entrypoint.sh
-  sed -i.bk "s/PYTHON_VERSION/$PYTHON_VERSION/g" $VERSION/Dockerfile
-  sed -i.bk "s/DEVPI_VERSION/$VERSION/g" $VERSION/requirements.txt
+  sed -i "s/PYTHON_VERSION/$PYTHON_VERSION/g" $VERSION/Dockerfile
+  sed -i "s/DEVPI_VERSION/$VERSION/g" $VERSION/requirements.txt
 }
 
 if [ -z $VERSION -o -z $PYTHON_VERSION ] ; then
