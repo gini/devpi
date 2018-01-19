04a78b76d5d8d1e5914b7a35e0134ee9bce7800e
David González Ruiz <david@gini.net>
2018-01-19 16:11:30 +0100

    Make server address configurable

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -29,6 +29,10 @@ elif [ -z "$DEVPI_ROOT_PASSWORD" ]; then
     DEVPI_ROOT_PASSWORD=$(generate_password)
 fi
 
+if [ -z "$SERVER_NAME" ] ; then
+    SERVER_NAME="http://localhost:3141"
+fi
+
 if [ ! -d "$DEVPI_SERVER_ROOT" ]; then
     echo "ENTRYPOINT: Creating devpi-server root"
     mkdir -p "$DEVPI_SERVER_ROOT"
