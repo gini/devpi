2ffd307e9e5d2bc5f6dde415af35a7f232d194bb
David González Ruiz <david@gini.net>
2018-01-19 14:20:17 +0100

    Sanity checks for env. variable presence

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-docker images 
+if [ -z $DOCKER_USER -o -z $DOCKER_PASSWORD ] ; then
+  echo "Missing env variables for pushing to the docker hub"
+  exit 2
+fi  
 
-docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 
 docker push gini/devpi:$VERSION
+
